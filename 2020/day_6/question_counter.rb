8733506cfbd5d8fb2b3626c880de157cd55fd05e
Gergely Domok <emanreyalp@users.noreply.github.com>
2020-12-06 13:01:47 +0100

    Day 6 / 2

--- 2020/day_6/question_counter.rb
+++ 2020/day_6/question_counter.rb
@@ -7,12 +7,16 @@ class QuestionCounter
 
   def read_answers(file_name)
     f = File.open(file_name).read
-    f.split("\n\n").map{ |l| l.tr("\n",'').chars }
+    f.split("\n\n").map{ |l| l.split }
   end
 
   def sum
     # require 'pry'; binding.pry
-    @answers.inject(0){ |sum,x| sum + x.uniq.count }
+    @answers.inject(0){ |sum,x| sum + x.inject([]){|s,a| s + a.chars}.uniq.count }
+  end
+
+  def other_sum
+    @answers.inject(0){ |sum,x| sum + x.inject(x.first.chars){|s,a| s = s & a.chars}.uniq.count }
   end
 end
 
